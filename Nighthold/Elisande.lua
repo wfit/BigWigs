92f4a902d5755cbfa6b9cadf55dc0066fddb8c74
Bastien Clément <g@ledric.me>
2017-01-31 13:35:47 +0100

    Elisande: change stage bar icon to "Zzzz"

--- Nighthold/Elisande.lua
+++ Nighthold/Elisande.lua
@@ -298,7 +298,7 @@ function mod:TimeStop()
 	end
 	phase = phase + 1
 	self:Message("stages", "Neutral", "Info", CL.phase:format(phase), false)
-	self:Bar("stages", 10, CL.stage:format(phase), 208863)
+	self:Bar("stages", 10, CL.stage:format(phase), 211647)
 end
 
 do
